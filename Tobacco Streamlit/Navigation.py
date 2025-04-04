7dc31512297726427571387591f797514b51f3a5
Me <owenmolloy23@hotmail.com>
2025-04-04 15:14:56 +0200

    Added conclusion to navigation

--- Tobacco Streamlit/Navigation.py
+++ Tobacco Streamlit/Navigation.py
@@ -7,6 +7,7 @@ exp_page = st.Page("Pages/03_Exploratory_Analysis.py", title = "Exploratory Anal
 pre_page =st.Page("Pages/04_Preprocessing.py", title = "Preprocessing", icon="🧹")
 ml_page = st.Page("Pages/05_ML_Modeling.py", title="Machine Learning", icon="🤖")
 stats_page = st.Page("Pages/06_Statistical_Modeling.py", title="Statistical Analysis", icon="📈")
+conc_page = st.Page("Pages/08_Conclusion_and_Outlook.py", title="Conclusion", icon="🏁")
 
-pg = st.navigation([home_page, intro_page, presentation_page, exp_page, pre_page, ml_page, stats_page])
+pg = st.navigation([home_page, intro_page, presentation_page, exp_page, pre_page, ml_page, stats_page, conc_page])
 pg.run()
